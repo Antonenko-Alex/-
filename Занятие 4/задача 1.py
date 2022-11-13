b2a8eed37b67b5ad2797196d5a61f039ff38f8df
Antonenko-Alex <114452167+Antonenko-Alex@users.noreply.github.com>
2022-11-13 18:10:08 +0300

    Update задача 1.py

--- Занятие 4/задача 1.py
+++ Занятие 4/задача 1.py
@@ -1,8 +1,10 @@
-a =int(input())
-b =int(input())
-if (a<=b):
-    for i in(a,b+1):
+def count(a,b):
+    a =int(input())
+    b =int(input())
+    if (a<=b):
+        for i in(a,b+1):
         print(i, sep='')
-else:
-    for i in(a,b-1,a+1):
+    else:
+      for i in(a,b-1,a+1):
         print(a-i, sep='')
+count(a,b)
