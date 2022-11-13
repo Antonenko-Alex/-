e0303773e5d9400b067aad7855e8ef8b26ffbfc8
Antonenko-Alex <114452167+Antonenko-Alex@users.noreply.github.com>
2022-11-13 18:27:14 +0300

    Update задача 8.py

--- Занятие 5/задача 8.py
+++ Занятие 5/задача 8.py
@@ -1,13 +1,15 @@
-p = int(input())
-c = 1
-b = 0
-while p!=0:
-    v=int(input())
-    p,v=v,p
-    if v==p:
-        c+=1
-    if c>b:
-        b=c
-    if p!=v:
-        c=1
-print(b)        
+def count(p):
+    p = int(input())
+    c = 1
+    b = 0
+    while p!=0:
+        v=int(input())
+        p,v=v,p
+        if v==p:
+            c+=1
+        if c>b:
+            b=c
+        if p!=v:
+            c=1
+    print(b)        
+count(p)
