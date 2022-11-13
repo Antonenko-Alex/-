3036305aa2d189d0ba82660d081241b278974f60
Antonenko-Alex <114452167+Antonenko-Alex@users.noreply.github.com>
2022-11-13 18:18:07 +0300

    Update задача 8.py

--- Занятие 4/задача 8.py
+++ Занятие 4/задача 8.py
@@ -1,6 +1,8 @@
-n = int(input())
-k=i=1
-for i in range(1, n+1):
-    for j in range(1, i + 1):
-        print(j, sep='', end='')
-    print('')
+def count(n):
+    n = int(input())
+    k=i=1
+    for i in range(1, n+1):
+        for j in range(1, i + 1):
+            print(j, sep='', end='')
+        print('')
+count(n)
