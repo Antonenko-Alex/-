ae6257bf1495aeb1e211d61522b45214e605e7bd
Antonenko-Alex <114452167+Antonenko-Alex@users.noreply.github.com>
2022-11-13 18:21:32 +0300

    Update задача 3.py

--- Занятие 5/задача 3.py
+++ Занятие 5/задача 3.py
@@ -1,7 +1,9 @@
-N = int(input())
-s = 1
-i = -1
-while s<=N:
-    i+=1
-    s*=2
-print(i,s*0,5)
+def count(N):
+    N = int(input())
+    s = 1
+    i = -1
+    while s<=N:
+        i+=1
+        s*=2
+    print(i,s*0,5)
+count(N)
