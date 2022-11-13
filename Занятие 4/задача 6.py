557b3a09fcaa5134f99b636216210e008f681a17
Antonenko-Alex <114452167+Antonenko-Alex@users.noreply.github.com>
2022-11-13 18:14:39 +0300

    Update задача 6.py

--- Занятие 4/задача 6.py
+++ Занятие 4/задача 6.py
@@ -1,5 +1,7 @@
-result = 1
-n = int(input())
-for i in range(1, n + 1):
-    result *= i
-print(result)
+def count(n):
+    result = 1
+    n = int(input())
+    for i in range(1, n + 1):
+        result *= i
+    print(result)
+count(n)
