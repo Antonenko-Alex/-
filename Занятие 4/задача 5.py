94d0df8c3c3256d10b3a22d75da491cd8a16f3c7
Antonenko-Alex <114452167+Antonenko-Alex@users.noreply.github.com>
2022-11-13 18:13:35 +0300

    Update задача 5.py

--- Занятие 4/задача 5.py
+++ Занятие 4/задача 5.py
@@ -1,5 +1,7 @@
-n = int(input())
-sum = 0
-for i in range(1, n + 1):
-    sum += i**3
-print(sum)
+def count(a,b):
+    n = int(input())
+    sum = 0
+    for i in range(1, n + 1):
+        sum += i**3
+        print(sum)
+count(a,b)
