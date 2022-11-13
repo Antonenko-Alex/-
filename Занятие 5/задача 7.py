ffacbd6f016671c5d79e7cce777d1558c2503def
Antonenko-Alex <114452167+Antonenko-Alex@users.noreply.github.com>
2022-11-13 18:25:45 +0300

    Update задача 7.py

--- Занятие 5/задача 7.py
+++ Занятие 5/задача 7.py
@@ -1,10 +1,12 @@
-i = int(input())
-a = 0
-b = 0
-k=i
-while i !=0:
+def count(i):
     i = int(input())
-    if i>k:
-        b+=1
+    a = 0
+    b = 0
     k=i
-print(b)
+    while i !=0:
+        i = int(input())
+        if i>k:
+            b+=1
+        k=i
+    print(b)
+count(i)
