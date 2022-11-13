00b0cf1ac3355d0035074d4aa6327009845c9d51
Antonenko-Alex <114452167+Antonenko-Alex@users.noreply.github.com>
2022-11-13 18:17:11 +0300

    Update задача 7.py

--- Занятие 4/задача 7.py
+++ Занятие 4/задача 7.py
@@ -1,7 +1,9 @@
-n = int(input())
-f = 1
-sum = 0
-for i in range(1, n + 1):
-    f = f * i
-    sum += f
-print(sum)
+def count(n):
+    n = int(input())
+    f = 1
+    sum = 0
+    for i in range(1, n + 1):
+        f = f * i
+        sum += f
+    print(sum)
+count(n)
