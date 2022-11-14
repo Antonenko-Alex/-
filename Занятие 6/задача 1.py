c759a084250b690cc503e23998ccb785fa15b76d
Antonenko-Alex <114452167+Antonenko-Alex@users.noreply.github.com>
2022-11-14 22:26:53 +0300

    Update задача 1.py

--- Занятие 6/задача 1.py
+++ Занятие 6/задача 1.py
@@ -1,6 +1,8 @@
+def tesk1('в лесу стояли ели')
 words = "в лесу стояли ели"
 count = 0
 for word in words.split(" "):
     if word.strip()[0] == 'е':
         count +=1
 print(count)
+tesk1('в лесу стояли ели')
